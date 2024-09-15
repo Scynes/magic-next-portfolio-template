e55e9d7437fe30e0608acd0e106ef056338cb339
Scynes <dustin.greyfield@gmail.com>
2024-09-15 11:02:20 -0400

    add: tooltips on hoving navigation links.

--- src/components/core/navigation/navigation-bar.tsx
+++ src/components/core/navigation/navigation-bar.tsx
@@ -9,34 +9,34 @@ export const NavigationBar = () => {
         <Flex className={ 'relative' } align={ 'center' } justify={ 'center' }>
             <Dock direction={ 'middle' } className={ 'rounded-full bg-[--color-panel] !gap-0' }>
                 <DockIcon>
-                    <NavigationLink href={ '/' }>
+                    <NavigationLink href={ '/' } label={ 'Home' }>
                         <FaHouse size={ '1.10rem' } />
                     </NavigationLink>
                 </DockIcon>
                 <DockIcon>
-                    <NavigationLink href={ '/' }>
+                    <NavigationLink href={ '/' } label={ 'Projects' }>
                         <FaFolderOpen size={ '1.10rem' } />
                     </NavigationLink>
                 </DockIcon>
                 <DockIcon>
-                    <NavigationLink href={ '/' }>
+                    <NavigationLink href={ '/' } label={ 'Blog' }>
                         <FaNewspaper size={ '1.10rem' } />
                     </NavigationLink>
                 </DockIcon>
                 <Separator orientation={ 'vertical' } size={ '4' } />
                 <DockIcon>
-                    <NavigationLink href={ '/' }>
+                    <NavigationLink href={ '/' } label={ 'GitHub' }>
                         <FaGithub size={ '1.10rem' } />
                     </NavigationLink>
                 </DockIcon>
                 <DockIcon>
-                    <NavigationLink href={ '/' }>
+                    <NavigationLink href={ '/' } label={ 'LinkedIn' }>
                         <FaLinkedin size={ '1.10rem' } />
                     </NavigationLink>
                 </DockIcon>
                 <Separator orientation={ 'vertical' } size={ '4' } />
                 <DockIcon>
-                    <NavigationLink href={ '/' }>
+                    <NavigationLink href={ '/' } label={ 'Dark Mode' }>
                         <IoSunny size={ '1.10rem' } />
                     </NavigationLink>
                 </DockIcon>
--- src/components/core/navigation/navigation-link.tsx
+++ src/components/core/navigation/navigation-link.tsx
@@ -1,9 +1,12 @@
+import { Tooltip } from '@radix-ui/themes'
 import Link from 'next/link'
 
-export const NavigationLink = ({ href, children }: { href: string, children: React.ReactNode }) => {
+export const NavigationLink = ({ href, label, children }: { href: string, label: string, children: React.ReactNode }) => {
     return (
-        <Link href={ href } className={ 'transition-all p-3 hover:bg-[--gray-3] rounded-full' }>
-            { children }
-        </Link>
+        <Tooltip content={ label }>
+            <Link href={ href } className={ 'transition-all p-3 hover:bg-[--gray-3] rounded-full' }>
+                { children }
+            </Link>
+        </Tooltip>
     )
 }
