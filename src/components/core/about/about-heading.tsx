764fb3b970ce6dfaff62c17a7afae0227fd27192
Scynes <dustin.greyfield@gmail.com>
2024-09-15 22:41:58 -0400

    add: scroll velocity magic UI component for about-heading.

--- src/components/core/about/about-heading.tsx
+++ src/components/core/about/about-heading.tsx
@@ -1,12 +1,16 @@
 import HyperText from '@/components/magicui/hyper-text';
+import { VelocityScroll } from '@/components/magicui/scroll-based-velocity';
 import { Flex, Heading } from '@radix-ui/themes';
 
 export const AboutHeading = () => {
     return (
-        <Flex gap={ '2' } justify={ 'between' } className={ 'flex-1' }>
-            <Flex direction={ 'column' }>
+        <Flex gap={ '2' } justify={ 'between' } className={ 'w-full' }>
+            <Flex direction={ 'column' } className={ 'overflow-x-hidden' }>
                 <Heading size={{ initial: '6', sm: '9' }}>
                     <HyperText text={ "Hi! I'm Dustin." } />
+                </Heading>
+                <Heading className={ 'overflow-hidden' }>
+                    <VelocityScroll text={ 'Full Stack Engineer | Frontend Expert | Master CS | Gamer | Skateboarder | Hackathon Winner |' } default_velocity={ 2 } />
                 </Heading>
             </Flex>
         </Flex>
