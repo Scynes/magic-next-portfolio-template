1cacee6d2cdbb34f290f9d8c1d6ee9c934fe29b3
Scynes <dustin.greyfield@gmail.com>
2024-09-15 23:39:17 -0400

    add: box reveal for intro about paragraphs.

--- src/components/core/about/about.tsx
+++ src/components/core/about/about.tsx
@@ -1,17 +1,22 @@
-import { Flex, Heading, Link, Text } from '@radix-ui/themes';
+import { Flex, Heading, Link } from '@radix-ui/themes';
 import { AboutHeading } from './about-heading';
 import AnimatedShinyText from '@/components/magicui/animated-shiny-text';
 import { Technology } from './technology';
+import BoxReveal from '@/components/magicui/box-reveal';
 
 export const About = () => {
     return (
         <Flex className={ 'min-h-dvh' } direction={ 'column' } gap={ '8' }>
             <AboutHeading />
             <Flex direction={ 'column' } gap={ '3' }>
-                <Heading weight={ 'bold' } size={{ initial: '4', sm: '6' }} className={ 'underline' }>ABOUT</Heading>
-                <AnimatedShinyText className={ 'text-gray-400 text-sm sm:text-lg' } shimmerWidth={ 200 }>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod delectus eveniet cumque nesciunt, odit non, <Link href={ '/' }>quasi quam laboriosam sint voluptatum beatae</Link> dicta labore eos voluptates. Dignissimos quam nesciunt ad culpa <Link href={ '/' }>repellat praesentium explicabo nihil</Link>. Libero aut provident quos voluptates incidunt.
-                </AnimatedShinyText>
+                <BoxReveal boxColor={ 'black' }>
+                    <Heading weight={ 'bold' } size={{ initial: '4', sm: '6' }} className={ 'underline' }>ABOUT</Heading>
+                </BoxReveal>
+                <BoxReveal boxColor={ 'black' }>
+                    <AnimatedShinyText className={ 'text-gray-400 text-sm sm:text-lg' } shimmerWidth={ 200 }>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod delectus eveniet cumque nesciunt, odit non, <Link href={ '/' }>quasi quam laboriosam sint voluptatum beatae</Link> dicta labore eos voluptates. Dignissimos quam nesciunt ad culpa <Link href={ '/' }>repellat praesentium explicabo nihil</Link>. Libero aut provident quos voluptates incidunt.
+                    </AnimatedShinyText>
+                </BoxReveal>
             </Flex>
             <Technology />
         </Flex>
