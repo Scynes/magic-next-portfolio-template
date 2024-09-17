274c0d23c86d032184ccb81a96f43dc3fe991cfc
Ellie Solhjou <elliesolhjou@gmail.com>
2024-09-17 17:15:13 -0500

    update name

--- src/components/core/about/about-heading.tsx
+++ src/components/core/about/about-heading.tsx
@@ -7,10 +7,10 @@ export const AboutHeading = () => {
         <Flex gap={ '2' } justify={ 'between' } className={ 'w-full' }>
             <Flex direction={ 'column' } className={ 'overflow-x-hidden' }>
                 <Heading size={{ initial: '7', sm: '9' }}>
-                    <HyperText text={ "Hi! I'm Dustin." } />
+                    <HyperText text={ "Hi! I'm Ellie." } />
                 </Heading>
                 <Heading size={{ initial: '5', sm: '7' }} className={ 'overflow-hidden' }>
-                    <VelocityScroll text={ 'Full Stack Engineer | Frontend Expert | Master CS | Gamer | Skateboarder | Hackathon Winner |' } default_velocity={ 2 } />
+                    <VelocityScroll text={ 'Full Stack Engineer | USC CS Student | Project Manager | Tennis Player |' } default_velocity={ 2 } />
                 </Heading>
             </Flex>
         </Flex>
