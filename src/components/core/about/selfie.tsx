13c3ba3f002a4ddef4396f728dbaf748e4f402ca
Scynes <dustin.greyfield@gmail.com>
2024-09-17 15:34:17 -0400

    style: some general styling changes to the appearance of the selfie image.

--- src/components/core/about/selfie.tsx
+++ src/components/core/about/selfie.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export const Selfie = () => {
     return (
         <BlurFade className={ 'self-center' } inView>
-            <Image priority className={ 'rounded-full sm:size-[300px]' } src={ '/images/selfie.jpg' } width={ 200 } height={ 200 } alt={ 'Profile picture avatar' } />
+            <Image priority className={ 'rounded-full sm:size-[300px] shadow-2xl shadow-[--accent-8] border-4' } src={ '/images/selfie.jpg' } width={ 200 } height={ 200 } alt={ 'Profile picture avatar' } />
         </BlurFade>
     );
 }
