105ec1a124782fe5a19e887d8630123e40d0e705
Ellie Solhjou <elliesolhjou@gmail.com>
2024-09-17 17:20:40 -0500

    updatee education

--- src/constants/education.ts
+++ src/constants/education.ts
@@ -2,18 +2,18 @@ import { Experience } from '@/types/experience';
 
 export const EDUCATION: Experience[] = [
     {
-        company: 'University A',
-        role: 'Bachelor of Science in Computer Science',
-        startDate: '2016-09-01',
-        endDate: '2020-06-01',
-        description: 'Studied various computer science topics including algorithms, data structures, and software engineering.',
+        company: 'University of Southern California',
+        role: 'Master of Science in Computer Science',
+        startDate: '2025',
+        endDate: '2027',
+        description: 'Studied various computer science topics including algorithms, data structures, databases and software engineering.',
         logoUrl: 'https://example.com/logo-university-a.png',
     },
     {
-        company: 'University B',
-        role: 'Master of Science in Software Engineering',
-        startDate: '2020-09-01',
-        endDate: '2022-06-01',
+        company: 'Texas A&M University',
+        role: 'Bachelor of Science in Engineering',
+        startDate: '2009-09-01',
+        endDate: '2014-06-01',
         description: 'Focused on advanced software development techniques and project management.',
         logoUrl: 'https://example.com/logo-university-b.png',
     }
