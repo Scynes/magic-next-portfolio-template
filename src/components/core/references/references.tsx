3be5c48db5876ec9cdfc4872eb694f9ae9c65e4d
Scynes <dustin.greyfield@gmail.com>
2024-09-16 20:03:45 -0400

    add: references section overhauled and added section header with magic ui sparkle text.

--- src/components/core/references/references.tsx
+++ src/components/core/references/references.tsx
@@ -1,24 +1,42 @@
 import Marquee from '@/components/magicui/marquee';
 import { REFERENCES } from '@/constants/references';
-import { Box } from '@radix-ui/themes';
+import { Box, Flex, Text } from '@radix-ui/themes';
 import { ReferenceCard } from './reference-card';
+import { SectionHeading } from '../section-heading';
+import SparklesText from '@/components/magicui/sparkles-text';
+import HyperText from '@/components/magicui/hyper-text';
+import BlurFade from '@/components/magicui/blur-fade';
 
 export const References = () => {
 
     return (
-        <Box className={ 'relative flex w-full flex-col items-center justify-center overflow-hidden' }>
-            <Marquee pauseOnHover className='[--duration:120s]'>
-                {REFERENCES.map((reference, index) => (
-                    <ReferenceCard key={index} reference={reference} />
-                ))}
-            </Marquee>
-            <Marquee reverse pauseOnHover className='[--duration:120s]'>
-                {REFERENCES.map((reference, index) => (
-                    <ReferenceCard key={index} reference={reference} />
-                ))}
-            </Marquee>
-            <Box className={ 'pointer-events-none absolute inset-y-0 left-0 w-1/5 bg-gradient-to-r from-white dark:from-black' } />
-            <Box className={ 'pointer-events-none absolute inset-y-0 right-0 w-1/5 bg-gradient-to-l from-white dark:from-black' } />
-        </Box>
+        <Flex direction={ 'column' } gap={ '5' }>
+            <Flex direction={ 'column' } align={ 'center' } justify={ 'center' }>
+                <BlurFade delay={ 0.2 * 1 } inView>
+                    <HyperText text={ "Don't just take my word for it." } className={ 'underline font-bold' } />
+                </BlurFade>
+                <BlurFade delay={ 0.2 * 2 } inView>
+                    <SparklesText text={ 'See What People Say.' } />
+                </BlurFade>
+            </Flex>
+            <Flex className={ 'w-full' } gap={ '4' } wrap={ 'wrap' } justify={ 'between' }>
+                <BlurFade delay={ 0.2 * 3 } className={ 'w-full' } inView>
+                    <Box className={ 'relative flex w-full flex-col items-center justify-center overflow-hidden' }>
+                        <Marquee pauseOnHover className='[--duration:120s]'>
+                            {REFERENCES.map((reference, index) => (
+                                <ReferenceCard key={index} reference={reference} />
+                            ))}
+                        </Marquee>
+                        <Marquee reverse pauseOnHover className='[--duration:120s]'>
+                            {REFERENCES.map((reference, index) => (
+                                <ReferenceCard key={index} reference={reference} />
+                            ))}
+                        </Marquee>
+                        <Box className={ 'pointer-events-none absolute inset-y-0 left-0 w-1/5 bg-gradient-to-r from-white dark:from-black' } />
+                        <Box className={ 'pointer-events-none absolute inset-y-0 right-0 w-1/5 bg-gradient-to-l from-white dark:from-black' } />
+                    </Box>
+                </BlurFade>
+            </Flex>
+        </Flex>
     );
 }
